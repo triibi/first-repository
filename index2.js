84371714f1983eb46d6460cfddf48ec0e48f5a53
buzilovadaria <buzilovadaria@gmail.com>
2025-12-21 13:23:40 +0400

    добавила ссылки

---
+++ index2.js
@@ -0,0 +1,27 @@
+/*document.addEventListener("DOMContentLoaded", function() {*/
+    const sliderTrack = document.querySelector(".slider-track");
+    const slides = document.querySelectorAll(".slide");
+    const halfLength = Math.floor(slides.length / 2); 
+    const changeButton = document.getElementById("changeButton");
+
+    let currentIndex = 0;
+
+    function moveToNext() {
+        currentIndex++;
+
+    
+        if (currentIndex === halfLength) {
+            sliderTrack.style.transitionDuration = '0ms';
+            sliderTrack.style.transform = `translateX(0px)`;
+            currentIndex = 0; 
+        } else {
+            sliderTrack.style.transitionDuration = '0.5s';
+        }
+
+       
+        const translateAmount = -(currentIndex * (slides[0].getBoundingClientRect().width + 40));
+        sliderTrack.style.transform = `translateX(${translateAmount}px)`;
+    }
+
+    changeButton.addEventListener("click", moveToNext);
+//});
